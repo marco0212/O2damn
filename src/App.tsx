2f652b0673551581ea1127d5f7d779eef529a8ca
Yobi <inyeop0212@gmail.com>
2022-06-29 17:12:08 +0900

    feat: firebase를 초기화한다

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import { initializeApp } from "firebase/app";
 
 function App() {
-  return (
-    <div>Hello</div>
-  );
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
+
+  initializeApp(firebaseConfig);
+
+  return <div>Hello</div>;
 }
 
 export default App;
