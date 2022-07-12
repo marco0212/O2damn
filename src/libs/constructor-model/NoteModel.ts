4d276558e6d7777a52d0438ceec255cbc2ac865f
Yobi <inyeop0212@gmail.com>
2022-07-12 22:27:47 +0900

    feat: Note Model을 구현한다

---
+++ src/libs/constructor-model/NoteModel.ts
@@ -0,0 +1,14 @@
+import { Model } from "./Model";
+
+export type Note = {
+  key: number;
+  time: number;
+};
+
+class NoteModel extends Model<Note[]> {
+  constructor() {
+    super("notes");
+  }
+}
+
+export const noteModel = new NoteModel();
